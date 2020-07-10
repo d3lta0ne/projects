839125afb6be8ae95de46f6db2a71099e910fae8
David Bruno <davidzbruno@protonmail.com>
2020-07-09 20:40:15 -0400

    Breaking Bad API Finished

--- breaking-bad-api/src/App.js
+++ breaking-bad-api/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/ui/Header";
+import Search from "./components/ui/Search";
 import CharacterGrid from "./components/characters/CharacterGrid";
 import "./App.css";
 import axios from "axios";
@@ -7,22 +8,24 @@ import axios from "axios";
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
       );
-      console.log(result.data);
+      // console.log(result.data);
       setItems(result.data);
       setIsLoading(false);
     };
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
