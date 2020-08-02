468acb728d31315d577148738a2a7ef9178ddb01
“David <davidzbruno@protonmail.com>
2020-08-02 03:12:43 -0400

    button functionality added

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,25 +1,50 @@
 // var sz = Number(prompt("How big is the grid?"));
-var sz = 5;
-var grid = document.getElementById("master-grid");
+let sz = 5;
+const GRID = document.getElementById("master-grid");
+const RESIZE = document.getElementById("resize");
+const RESET = document.getElementById("reset");
 
+// Function that creates the grid space
 createGrid = () => {
-    grid.style.setProperty(
-        "grid-template-columns",
-        `repeat(${sz}, 2fr)`
-      );
-      grid.style.setProperty(
-        "grid-template-rows",
-        `repeat(${sz}, 2fr)`
-      );
+  GRID.style.setProperty("grid-template-columns", `repeat(${sz}, 1fr)`);
+  GRID.style.setProperty("grid-template-rows", `repeat(${sz}, 1fr)`);
 
-    for (let i = 0; i < sz*sz; i++) {        
-      const cells = document.createElement("div");
-      cells.classList.add("cell")
-      cells.addEventListener("mouseover", function(event) {
-        event.target.classList.toggle("color");
+  for (let i = 0; i < sz * sz; i++) {
+    const cells = document.createElement("div");
+    cells.classList.add("cell");
+    cells.addEventListener("mouseover", function (event) {
+      event.target.classList.toggle("color");
     });
-      grid.appendChild(cells);
-    }
-  };
+    GRID.appendChild(cells);
+  }
+};
+// console.log(GRID.getElementsByClassName("cell"));
+
+//Function to clear the board on the grid space
+resetGrid = () => {
+  let grid = GRID.getElementsByClassName("cell");
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].classList.remove("color");
+  }
+};
+
+// Function to remove all the cells in the grid
+clearGrid = () => {
+  let grid = GRID;
+  while (GRID.firstChild) {
+    GRID.removeChild(GRID.lastChild);
+  }
+};
+
+//Funciton to resize the size of the current grid
+resizeGrid = () => {
+  clearGrid();
+  sz = 10;
+  createGrid();
+};
 
 createGrid();
+
+//Add Event listeners to the buttons
+RESET.addEventListener("click", resetGrid);
+RESIZE.addEventListener("click", resizeGrid);
