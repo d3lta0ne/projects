2f5dc8a06a534f7534f810f7f6124013e2d8b777
“David <davidzbruno@protonmail.com>
2020-08-23 16:06:05 -0400

    project init --tailwind added

---
+++ whatsapp-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-red-600 bg-red p-5">
+        Let' dfs s Build Whatsapp Clone Black
+      </p>
+
+      <div className="bg-red500 w-5 h-5">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
