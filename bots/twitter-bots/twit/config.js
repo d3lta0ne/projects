cf69c04d7bf7341d7267fa625a415463f21ca0fb
James Renfrow <39011780+d3lta0ne@users.noreply.github.com>
2023-12-29 20:12:31 +0000

    Delete bots directory

--- bots/twitter-bots/twit/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  consumer_key: "uFcZxAUsYjuhvxybWgqy5VqSE",
-  consumer_secret: "H1y04WojSDb1sNqv6IlDxtVYlotspGvyY96upiWdxYi3q9hPHC",
-  access_token: "1282648692261093376-rOcebVrWOlXdVfvW8OD5dFUiAWEStf",
-  access_token_secret: "nWkjhlWjWPvdWxeT8asDUEMAXdNuvIDgnMmGt0G7f6j6x",
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true, // optional - requires SSL certificates to be valid.
-};
